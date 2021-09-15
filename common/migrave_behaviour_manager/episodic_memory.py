3b667ddcd8c555d09fadef4b600bf8b0b75e1935
Alex Mitrevski <aleksandar.mitrevski@h-brs.de>
2021-09-15 16:56:00 +0200

    Added skeleton modules for episodic and procedural memories

---
+++ common/migrave_behaviour_manager/episodic_memory.py
@@ -0,0 +1,8 @@
+class EpisodicMemory(object):
+    def __init__(self):
+        pass
+
+    def get_action_for_state(self, state):
+        # TODO: retrieve memories from database
+        # to check if the state is known
+        return None
---
+++ common/migrave_behaviour_manager/procedural_memory.py
@@ -0,0 +1,9 @@
+class ProceduralMemory(object):
+    def __init__(self):
+        pass
+
+    def get_possible_actions(self, state):
+        # TODO: check which actions are possible in the current state
+        possible_actions = []
+        action_preferences = []
+        return (possible_actions, action_preferences)
