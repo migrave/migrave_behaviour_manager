cc0636ac2831b865a2444f44b5377b74d0741ce4
Michal <mlk04@wp.pl>
2021-09-15 18:11:04 +0200

    [action_management] Action interface

---
+++ common/src/migrave_behaviour_manager/action_interface.py
@@ -0,0 +1,40 @@
+import os
+import yaml
+import rospy
+
+class ActionInterface():
+    'The purpose of this class is to fetch actions defined in the config/actions directory'
+    
+    def __init__(self):
+        self.actions = []
+
+        # TO-DO make reconfigurable
+        self.path = '../../../config/actions.yaml'
+
+        self.refresh()
+
+    def refresh(self):
+        self.actions = self.fetch_actions()
+
+    def fetch_actions(self):
+        with open(self.path, "r") as config:
+            try:
+                actions = yaml.safe_load(config)
+                
+            except yaml.YAMLError as exc:
+                rospy.logerr(exc)
+        
+        return actions
+
+    def get_actions(self):
+        return self.actions
+
+
+    def get_action(self, action_name):
+        return [action for action in self.actions if action_name == action['name']][0]
+
+    
+# if __name__ == '__main__':
+#     actionint = ActionInterface()
+#     print(actionint.get_actions())
+#     print(actionint.get_action('self_introduce'))
