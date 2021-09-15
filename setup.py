8058568d6845dd6d7830cf6afe8124cc21ac4dae
Alex Mitrevski <aleksandar.mitrevski@h-brs.de>
2021-09-15 15:52:48 +0200

    Added package skeleton

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+d = generate_distutils_setup(
+    packages=['migrave_behaviour_manager', 'migrave_behaviour_manager_wrapper'],
+    package_dir={'migrave_behaviour_manager': 'common/migrave_behaviour_manager',
+                 'migrave_behaviour_manager_wrapper': 'ros/src/migrave_behaviour_manager_wrapper'}
+)
+
+setup(**d)
